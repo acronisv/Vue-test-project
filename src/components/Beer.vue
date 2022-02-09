ce063f5c00f18cc0d5cbcf0094956d97626ea884
acronisv <acronisv@gmail.com>
2022-02-09 14:17:27 +0300

    Initial commit

---
+++ src/components/Beer.vue
@@ -0,0 +1,128 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col><h3>Beer description</h3></b-col>
+    </b-row>
+    <b-row class="beer-container">
+      <b-col md="4">
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Brand</p>
+          <span>{{ beerData.brand }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Name</p>
+          <span>{{ beerData.name }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Style</p>
+          <span> {{ beerData.style }}</span>
+        </div>
+      </b-col>
+
+      <b-col md="4">
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Hop</p>
+          <span>{{ beerData.hop }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Yeast</p>
+          <span>{{ beerData.yeast }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Malts</p>
+          <span>{{ beerData.malts }}</span>
+        </div>
+      </b-col>
+
+      <b-col md="4">
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Bitterness</p>
+          <span>{{ beerData.ibu }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Alcohol</p>
+          <span>{{ beerData.alcohol }}</span>
+        </div>
+
+        <div class="beer-feature">
+          <p class="beer-feature__caption">Sugar</p>
+          <span>{{ beerData.blg }}</span>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row class="btn-container">
+      <b-col>
+        <b-button variant="outline-primary" v-on:click="methodThatForcesUpdate()">Take a new one</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      beerData: {
+        id: 9153,
+        uid: "59115655-8da8-4210-9ed8-010a0269d8e4",
+        brand: "Pabst Blue Ribbon",
+        name: "Hercules Double IPA",
+        style: "Stout",
+        hop: "Brewer’s Gold",
+        yeast: "1028 - London Ale",
+        malts: "Munich",
+        ibu: "39 IBU",
+        alcohol: "6.1%",
+        blg: "13.3°Blg",
+      },
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      fetch("https://random-data-api.com/api/beer/random_beer")
+        .then((response) => response.json())
+        .then((json) => (this.beerData = json));
+    },
+    methodThatForcesUpdate() {
+      this.$forceUpdate();
+      this.getData();
+    },
+  },
+};
+</script>
+
+<style>
+.beer-container {
+  padding: 1.25rem
+}
+.beer-feature {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.beer-feature__caption {
+  font-weight: 700;
+}
+
+.beer-description {
+  text-align: left;
+  font-weight: 700;
+  flex-basis: 30%;
+}
+.beer-data {
+  flex-basis: 70%;
+  text-align: left;
+}
+.btn-container {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="container">
+    <h2>User profile</h2>
+    <b-tabs pills card justified >
+      <b-tab title="User" active>
+        <b-card-text>
+          <b-row>
+            <b-col>
+              <h3>{{ userData.first_name }} {{ userData.last_name }}</h3>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col md="6">
+              <img :src=userData.avatar alt="">
+            </b-col>
+            <b-col md="6" class="user-tab">
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Username</p>
+                <span class="user-tab__line-data">{{ userData.username }}</span>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">E-mail</p>
+                <a class="user-tab__line-data" :href="`mailto:${userData.email}`">{{ userData.email }}</a>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Phone</p>
+                <a class="user-tab__line-data" :href="`tel:${userData.phone_number}`">{{ userData.phone_number }}</a>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Gender</p>
+                <span class="user-tab__line-data">{{ userData.gender }}</span>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Insurance</p>
+                <span class="user-tab__line-data">{{ userData.social_insurance_number }}</span>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Birthday</p>
+                <span class="user-tab__line-data">{{ userData.date_of_birth }}</span>
+              </div>
+            </b-col>
+          </b-row>
+      </b-card-text>
+      </b-tab>
+      <b-tab title="Address">
+        <b-card-text>
+          <b-row>
+            <b-col md="6" class="user-tab">
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">City</p>
+                  <span class="user-tab__line-data">{{ userData.address.city }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Street</p>
+                  <span class="user-tab__line-data">{{ userData.address.street_name }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Address</p>
+                  <span class="user-tab__line-data">{{ userData.address.street_address }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Zip code</p>
+                  <span class="user-tab__line-data">{{ userData.address.zip_code }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">State</p>
+                  <span class="user-tab__line-data">{{ userData.address.state }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Country</p>
+                  <span class="user-tab__line-data">{{ userData.address.country }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Latitude</p>
+                  <span class="user-tab__line-data">{{ userData.address.coordinates.lat }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Longitude</p>
+                  <span class="user-tab__line-data">{{ userData.address.coordinates.lng }}</span>
+                </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+      </b-tab>
+      <b-tab title="Employment">
+        <b-card-text>
+          <b-row>
+            <b-col md="6" class="user-tab">
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Title</p>
+                <span class="user-tab__line-data">{{ userData.employment.title }}</span>
+              </div>
+              <div class="user-tab__line">
+                <p class="user-tab__caption">Skills</p>
+                <span class="user-tab__line-data">{{ userData.employment.key_skill }}</span>
+              </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+      </b-tab>
+      <b-tab title="Payment">
+        <b-card-text>
+          <b-row>
+            <b-col md="6" class="user-tab">
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Credit card</p>
+                  <span class="user-tab__line-data">{{ userData.credit_card.cc_number }}</span>
+                </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+      </b-tab>
+      <b-tab title="Subscription">
+         <b-card-text>
+           <b-row>
+            <b-col md="6" class="user-tab">
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Plan</p>
+                  <span class="user-tab__line-data">{{ userData.subscription.plan }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Status</p>
+                  <span class="user-tab__line-data">{{ userData.subscription.status }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Payment method</p>
+                  <span class="user-tab__line-data">{{ userData.subscription.payment_method }}</span>
+                </div>
+                <div class="user-tab__line">
+                  <p class="user-tab__caption">Terms</p>
+                  <span class="user-tab__line-data">{{ userData.subscription.term }}</span>
+                </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      userData: [],
+    };
+  },
+  mounted() {
+    fetch("https://random-data-api.com/api/users/random_user")
+      .then((response) => response.json())
+      .then((json) => (this.userData = json));
+  },
+};
+</script>
+<style>
+.user-tab {
+  display: flex;
+  flex-direction: column;
+  margin: 10px auto;
+}
+.user-tab__line {
+  display: flex;
+  text-align: left;
+}
+
+.user-tab__caption {
+  font-weight: 700;
+  flex-basis: 30%;
+}
+
+.user-tab__line-data {
+  margin: 0 0 0 auto;
+  word-wrap: break-word;
+  overflow: hidden;
+  text-align: right;
+}
+
+</style>
